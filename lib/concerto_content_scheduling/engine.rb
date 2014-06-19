27633f54040f4f0e241e204961baa6f2beb7e0eb
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-06-18 16:55:46 -0800

    support filtering based on the schedule

--- lib/concerto_content_scheduling/engine.rb
+++ lib/concerto_content_scheduling/engine.rb
@@ -10,7 +10,13 @@ module ConcertoContentScheduling
         add_view_hook "ContentsController", 
                       :content_form, 
                       :partial => "concerto_content_scheduling/content/schedule"
-        
+
+        # filter the contents according to the schedule
+        # TODO test with the manykinds plugin concurrently enabled to see how it works
+        add_controller_hook "Subscription", :filter_contents, :after do
+          @contents.reject!{|c| !is_effective(c.start_time, c.schedule)}
+        end
+
       end
     end
     
