d938faf8b36ef3749625ced65cf30fb194b47dd4
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-06-18 17:22:59 -0800

    trying to get the is_effective? method to work

--- app/helpers/concerto_content_scheduling/application_helper.rb
+++ app/helpers/concerto_content_scheduling/application_helper.rb
@@ -1,29 +1,4 @@
 module ConcertoContentScheduling
   module ApplicationHelper
-
-    def is_effective?(start_time, schedule)
-      effective = false
-
-      # if no schedule set assume always available
-      if schedule.empty? or start_time.empty? or schedule['from_time'].empty? or schedule['to_time'].empty?
-        effective = true
-      else
-        # check the schedule... and see if it is within the viewing window for the day
-        if Clock.time >= Time.parse(schedule['from_time']) && Clock.time <= Time.parse(schedule['to_time'])
-          # and it matches the criteria
-          if !schedule['criteria'].empty?
-            s = IceCube::Schedule.new(start_time)
-            s.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(schedule['criteria']))
-            effective = s.occurs_on? Clock.time
-          else
-            # or no criteria was set
-            effective = true
-          end
-        end
-      end
-
-      effective
-    end
-
   end
 end
--- lib/concerto_content_scheduling/engine.rb
+++ lib/concerto_content_scheduling/engine.rb
@@ -12,12 +12,38 @@ module ConcertoContentScheduling
                       :partial => "concerto_content_scheduling/content/schedule"
 
         # filter the contents according to the schedule
-        # TODO test with the manykinds plugin concurrently enabled to see how it works
+        # the manykinds plugin if concurrently enabled will cause this to be skipped
         add_controller_hook "Subscription", :filter_contents, :after do
-          @contents.reject!{|c| !is_effective(c.start_time, c.schedule)}
+Rails.logger.debug('content scheduling filtering occurred -----------------------===========================================')          
+          @contents.reject!{|c| !is_effective?(c.start_time, c.schedule)}
         end
 
       end
+    end
+    
+
+    def is_effective?(start_time, schedule)
+      effective = false
+
+      # if no schedule set assume always available
+      if schedule.empty? or start_time.empty? or schedule['from_time'].empty? or schedule['to_time'].empty?
+        effective = true
+      else
+        # check the schedule... and see if it is within the viewing window for the day
+        if Clock.time >= Time.parse(schedule['from_time']) && Clock.time <= Time.parse(schedule['to_time'])
+          # and it matches the criteria
+          if !schedule['criteria'].empty?
+            s = IceCube::Schedule.new(start_time)
+            s.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(schedule['criteria']))
+            effective = s.occurs_on? Clock.time
+          else
+            # or no criteria was set
+            effective = true
+          end
+        end
+      end
+
+      effective
     end
     
   end
