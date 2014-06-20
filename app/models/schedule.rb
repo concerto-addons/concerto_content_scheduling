6f7c9732e9d6f550753a750607f361ccc9ec8b0f
Gabe Perez <perez283@gmail.com>
2014-06-20 17:06:56 -0400

    Remove old model

--- app/models/schedule.rb
+++
@@ -1,8 +0,0 @@
-module ConcertoContentScheduling
-  class Schedule < ActiveRecord::Base
-
-    SCHEDULE_RULES = ["Daily", "Weekly", "Monthly (by day of month)",
-                      "Yearly (by day of year)", "Yearly (by month of year)" ]
-
-  end
-end
